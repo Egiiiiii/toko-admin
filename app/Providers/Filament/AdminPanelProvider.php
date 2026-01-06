d06a2f72242338dba18f1738acdca8c885b20212
Egiiiiii <titanegif@gmail.com>
2026-01-06 13:02:31 +0700

    edit domain

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->domain('admin.roomify3.my.id')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
