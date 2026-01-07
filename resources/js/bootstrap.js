2c1c5be4d1fd53d53275ddb1e437d8d1158df559
Egiiiiii <titanegif@gmail.com>
2026-01-07 09:31:48 +0700

    update axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// INI BAGIAN PENTINGNYA:
+// Pastikan Base URL mengambil dari VITE_API_BASE_URL
+window.axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'https://api.roomify3.my.id'; 
+
+// Cek kredensial (penting jika domain frontend beda dengan backend)
+window.axios.defaults.withCredentials = true;
